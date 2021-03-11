223d4c2c69586d77bf29b2a20e1e2ebb83321584
ErenYaylaci <ErenYaylaci@DESKTOP-QUJIPHN>
2021-03-11 16:15:28 +0300

    main

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,33 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import { HttpClient } from '@angular/common/http';
 import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
-
-    products: Product[] = [];
-    productResponseModel: ProductResponseModel = {
+  products: Product[] = [];
+  productResponseModel: ProductResponseModel = {
     data: this.products,
-    message: "",
-    success: true
+    message: '',
+    success: true,
   };
-  apiUrl = "https://localhost:44399/api/products/getall";
-  constructor(private httpClient: HttpClient) { }
+  dataLoaded = false;
+  
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    console.log("product çalıştı");
+    console.log('product çalıştı');
     this.getProducts();
   }
-
-  getProducts() {
-    this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe((response) => {
+    
+  getProducts(){
+    this.productService.getProducts().subscribe(response => {
       this.products = response.data;
+      this.dataLoaded = true;
     });
+
   }
+  
 }
