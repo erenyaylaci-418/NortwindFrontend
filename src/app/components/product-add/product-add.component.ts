b73bff9bc3236d35c08d42a50b329e4fe440ccf0
ErenYaylaci <ErenYaylaci@DESKTOP-QUJIPHN>
2021-03-22 20:08:57 +0300

    .

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -35,10 +35,17 @@ export class ProductAddComponent implements OnInit {
         console.log(response)
         this.toastrService.success(response.message,"Başarılı")
       },responseError=> {
-        this.toastrService.error(responseError.error)
+        if (responseError.error.Errors.length>0) {
+          for (let i = 0; i < responseError.error.Errors.length; i++) {
+            
+            this.toastrService.error(responseError.error.Errors[i].ErrorMessage,"Doğrulama Hatası")
+          }
+          
+        }
+        
       });
     
-      console.log(producttModel)
+     
     }
     else
     {
